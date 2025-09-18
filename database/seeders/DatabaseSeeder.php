ecd99a86e67e0ded3a1ff2500dd59b0d79e9b9b9
Dilshodbek <ruzimurodovdilshod9@gmail.com>
2025-09-18 14:31:43 +0200

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,17 +9,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PermissionsTableSeeder::class,
-            RolesTableSeeder::class,
-            PermissionRoleTableSeeder::class,
-            UsersTableSeeder::class,
-            RoleUserTableSeeder::class,
+//            PermissionsTableSeeder::class,
+//            RolesTableSeeder::class,
+//            PermissionRoleTableSeeder::class,
+//            UsersTableSeeder::class,
+//            RoleUserTableSeeder::class,
 //            SampleData::class,
 //        StatisticsTableSeeder::class
-<<<<<<< HEAD
-=======
+
         SectionsTableSeeder::class
->>>>>>> 899a13156fc68b212d74217b0da706aef3dee2de
         ]);
     }
 }
